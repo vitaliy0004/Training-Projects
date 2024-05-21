36f9cda1d626b746ec1d4352c51ea60b1d6a89b5
Виталий Дымов <dymovvs11@gmail.com>
2024-05-21 15:43:44 +0300

    m_1_09

---
+++ m_1_09_animation_navigation/app/src/main/java/com/example/m_1_09_animation_navigation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.m_1_09_animation_navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.m_1_09_animation_navigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
